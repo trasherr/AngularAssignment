e51a67919063c878dcbc352bd1d084e4c61afba2
Prakhar Singh <68940515+trasherr@users.noreply.github.com>
2023-07-05 16:37:02 +0530

    Update studentRoute.js

--- Node/routes/studentRoute.js
+++ Node/routes/studentRoute.js
@@ -1,6 +1,6 @@
 import express  from "express";
 
-import { getStudent } from "../controllers/StudentController.js";
+import { getStudent } from "../controllers/studentController.js";
 
 const StudentRoute = express.Router();
 StudentRoute.get('/getStudent', getStudent );
